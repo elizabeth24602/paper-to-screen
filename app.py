26706bfd3a348fe98096033132770ff114f42671
Ubuntu <ubuntu@ip-172-31-87-171.ec2.internal>
2020-02-26 12:21:20 +0000

    adding task manager to forum1.html in templates

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId 
 
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'book_manager'
+app.config["MONGO_URI"] = ('mongodb+srv://Elizabeth24602:gavroche24602@myfirstcluster-xtdqk.mongodb.net/book_manager?retryWrites=true&w=majority')
+
+mongo = PyMongo(app)
 
 
 @app.route('/')
-def hello():
-    return 'Hello World ...again'
+@app.route('/get_book')
+def get_book():
+    return render_template("forum1.html", book=mongo.db.book.find())
 
 
 if __name__ == '__main__':

4b3baab8458718ca6054dce3258b918e9140a609
Ubuntu <ubuntu@ip-172-31-87-171.ec2.internal>
2020-02-26 15:09:42 +0000

    form template from materialize added to addbook.html

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
+from os import path
+if path.exists("env.py"):
+    import env
+
+
 import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
@@ -5,7 +10,7 @@ from bson.objectid import ObjectId
 
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'book_manager'
-app.config["MONGO_URI"] = ('mongodb+srv://Elizabeth24602:gavroche24602@myfirstcluster-xtdqk.mongodb.net/book_manager?retryWrites=true&w=majority')
+app.config["MONGO_URI"] = ('mongodb+srv://Elizabeth24602:Gavroche24602@myfirstcluster-xtdqk.mongodb.net/book_manager?retryWrites=true&w=majority')
 
 mongo = PyMongo(app)
 
@@ -14,6 +19,17 @@ mongo = PyMongo(app)
 @app.route('/get_book')
 def get_book():
     return render_template("forum1.html", book=mongo.db.book.find())
+    
+@app.route('/add_book')
+def add_book():
+    return render_template('addbook.html',
+                            categories=mongo.db.categories.find())
+
+@app.route('/insert_book', methods=['POST'])
+def insert_book():
+    tasks = mongo.db.book
+    tasks.insert_one(request.form.to_dict())
+    return redirect(url_for('get_book'))
 
 
 if __name__ == '__main__':

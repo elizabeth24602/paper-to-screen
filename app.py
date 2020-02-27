d445b5d805cd40527081dd2751f1023eeb980209
Ubuntu <ubuntu@ip-172-31-87-171.ec2.internal>
2020-02-27 16:32:11 +0000

    struggling to get URL linked correctly

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId 
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'book_manager'
@@ -14,9 +15,20 @@ app.config["MONGO_URI"] = ('mongodb+srv://Elizabeth24602:Gavroche24602@myfirstcl
 
 mongo = PyMongo(app)
 
-
 @app.route('/')
-@app.route('/get_book')
+@app.route('/index1')
+def index():      
+    return render_template("index1.html") 
+    
+@app.route('/about1')
+def about():      
+    return render_template("about1.html") 
+
+@app.route('/choices1')
+def choices():      
+    return render_template("choices1.html") 
+
+@app.route('/forum1')
 def get_book():
     return render_template("forum1.html", book=mongo.db.book.find())
     
@@ -30,6 +42,13 @@ def insert_book():
     tasks = mongo.db.book
     tasks.insert_one(request.form.to_dict())
     return redirect(url_for('get_book'))
+    
+@app.route('/edit_task/<task_id>')
+def edit_task(task_id):
+    the_task =  mongo.db.book.find_one({"_id": ObjectId(task_id)})
+    all_categories =  mongo.db.categories.find()
+    return render_template('editbook.html', task=the_task,
+                           categories=all_categories)
 
 
 if __name__ == '__main__':

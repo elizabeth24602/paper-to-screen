94efe6aa2b42d8b3d2e605b2fcff30e883c3b312
Ubuntu <ubuntu@ip-172-31-87-171.ec2.internal>
2020-03-16 10:50:15 +0000

    updated requirements.txt

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
+import os
+
 from os import path
 if path.exists("env.py"):
     import env
 
 
-import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
